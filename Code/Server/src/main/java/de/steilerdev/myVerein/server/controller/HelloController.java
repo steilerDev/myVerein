79be95b15fdda7677119a9db67f56f9a0e89ab33
Frank Steiler <frank@steilerdev.de>
2014-11-14 12:28:31 +0100

    General database connection working. * Added java spring mongo data dependency * Added database configuration * Added user model * Inserted PoC code within controller

--- Code/Server/src/main/java/de/steilerdev/myVerein/server/controller/HelloController.java
+++ Code/Server/src/main/java/de/steilerdev/myVerein/server/controller/HelloController.java
@@ -1,16 +1,27 @@
 package de.steilerdev.myVerein.server.controller;
 
+import de.steilerdev.myVerein.server.model.User;
+import de.steilerdev.myVerein.server.model.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.HashMap;
+
 @Controller
 @RequestMapping("/")
 public class HelloController {
+
+	@Autowired
+	private UserRepository userRepository;
+
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
+		User user1 = userRepository.findByFirstName("Frank");
 		model.addAttribute("message", "Hello world!");
+		model.addAttribute("user", user1);
 		return "hello";
 	}
 }
---
+++ Code/Server/src/main/java/de/steilerdev/myVerein/server/model/User.java
@@ -0,0 +1,125 @@
+/**
+ * Copyright (C) ${YEAR} Frank Steiler <frank@steilerdev.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.steilerdev.myVerein.server.model;
+
+import org.springframework.data.annotation.Id;
+
+import java.util.HashMap;
+
+public class User
+{
+    @Id
+    private String id;
+
+    private String firstName;
+    private String lastName;
+
+    private String email;
+    private String password;
+
+    private HashMap<String,String> privateInformation;
+    private HashMap<String,String> publicInformation;
+
+    public User() {}
+
+    public User(String firstName, String lastName, String email, String password, HashMap<String,String> privateInformation, HashMap<String,String> publicInformation)
+    {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.privateInformation = privateInformation;
+        this.publicInformation = publicInformation;
+    }
+
+    @Override
+    public String toString()
+    {
+        return String.format("User[id=%s, firstName=%s, lastName=%s", id, firstName, lastName);
+    }
+
+    public String getId()
+    {
+        return id;
+    }
+
+    public void setId(String id)
+    {
+        this.id = id;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+
+    public HashMap<String, String> getPrivateInformation()
+    {
+        return privateInformation;
+    }
+
+    public void setPrivateInformation(HashMap<String, String> privateInformation)
+    {
+        this.privateInformation = privateInformation;
+    }
+
+    public HashMap<String, String> getPublicInformation()
+    {
+        return publicInformation;
+    }
+
+    public void setPublicInformation(HashMap<String, String> publicInformation)
+    {
+        this.publicInformation = publicInformation;
+    }
+}
