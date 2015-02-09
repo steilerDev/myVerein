07c677681f7804d2205056afdc230162fa2994d1
Frank Steiler <frank@steilerdev.de>
2015-02-09 20:38:07 +0100

    Outsourced english language for the init page.

--- Code/Server/src/main/java/de/steilerdev/myVerein/server/controller/InitController.java
+++ Code/Server/src/main/java/de/steilerdev/myVerein/server/controller/InitController.java
@@ -16,19 +16,16 @@
  */
 package de.steilerdev.myVerein.server.controller;
 
-import com.mongodb.MongoException;
 import de.steilerdev.myVerein.server.model.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.ConstraintViolationException;
 import java.io.IOException;
@@ -66,11 +63,11 @@ public class InitController
         if(!settingsRepository.isInitSetup())
         {
             logger.warn("An initial setup API was used, even though the system is already configured.");
-            return new ResponseEntity<>(messageSource.getMessage("init.message.notAllowed", null, "You are not allowed to perform this action at the moment", locale), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(messageSource.getMessage("init.message.settings.notAllowed", null, "You are not allowed to perform this action at the moment", locale), HttpStatus.BAD_REQUEST);
         } else if(clubName.isEmpty() || rememberMeTokenKey.isEmpty())
         {
             logger.warn("The club name or remember me key is not present");
-            return new ResponseEntity<>(messageSource.getMessage("init.message.noKeyOrName", null, "The club name and remember me key is required", locale), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(messageSource.getMessage("init.message.settings.noKeyOrName", null, "The club name and remember me key is required", locale), HttpStatus.BAD_REQUEST);
         } else
         {
             if(databaseHost.isEmpty())
@@ -90,7 +87,7 @@ public class InitController
                 } catch (NumberFormatException e)
                 {
                     logger.warn("The database port seems not to be a number " + databasePort);
-                    return new ResponseEntity<>(messageSource.getMessage("init.message.dbPortNoNumber", null, "The database port needs to be a number", locale), HttpStatus.BAD_REQUEST);
+                    return new ResponseEntity<>(messageSource.getMessage("init.message.settings.dbPortNoNumber", null, "The database port needs to be a number", locale), HttpStatus.BAD_REQUEST);
                 }
             }
             if(databaseCollection.isEmpty())
@@ -112,9 +109,9 @@ public class InitController
             } catch (IOException e)
             {
                 logger.warn("Unable to save settings.");
-                return new ResponseEntity<>(messageSource.getMessage("init.message.savingSettingsError", null, "Unable to save settings", locale), HttpStatus.INTERNAL_SERVER_ERROR);
+                return new ResponseEntity<>(messageSource.getMessage("init.message.settings.savingSettingsError", null, "Unable to save settings, please try again", locale), HttpStatus.INTERNAL_SERVER_ERROR);
             }
-            return new ResponseEntity<>(messageSource.getMessage("init.message.savingSettingsSuccess", null, "Successfully saved settings", locale), HttpStatus.OK);
+            return new ResponseEntity<>(messageSource.getMessage("init.message.settings.savingSettingsSuccess", null, "Successfully saved settings", locale), HttpStatus.OK);
         }
     }
 
@@ -129,15 +126,15 @@ public class InitController
         if(!settingsRepository.isInitSetup())
         {
             logger.warn("An initial setup API was used, even though the system is already configured.");
-            return new ResponseEntity<>(messageSource.getMessage("init.message.notAllowed", null, "You are not allowed to perform this action at the moment", locale), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(messageSource.getMessage("init.message.admin.notAllowed", null, "You are not allowed to perform this action at the moment", locale), HttpStatus.BAD_REQUEST);
         } else if(firstName.isEmpty() || lastName.isEmpty() || email.isEmpty() || password.isEmpty() || passwordRe.isEmpty())
         {
             logger.warn("A required parameter of the super admin is empty or missing during initial configuration");
-            return new ResponseEntity<>("A required parameter is empty or missing", HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(messageSource.getMessage("init.message.admin.missingParameter", null, "A required parameter is empty or missing", locale), HttpStatus.BAD_REQUEST);
         } else if(!password.equals(passwordRe))
         {
             logger.warn("The password and the re-typed password do not match!");
-            return new ResponseEntity<>("The password and the re-typed password do not match!", HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(messageSource.getMessage("init.message.admin.passwordMatchError", null, "The password and the re-typed password do not match", locale), HttpStatus.BAD_REQUEST);
         } else
         {
             logger.debug("Creating a new initial user.");
@@ -159,7 +156,7 @@ public class InitController
             } catch (IOException e)
             {
                 logger.warn("Unable to save settings.");
-                return new ResponseEntity<>("Unable to save settings, please try again.", HttpStatus.INTERNAL_SERVER_ERROR);
+                return new ResponseEntity<>(messageSource.getMessage("init.message.admin.savingSettingsError", null, "Unable to save settings, please try again", locale), HttpStatus.INTERNAL_SERVER_ERROR);
             }
 
             try
@@ -169,9 +166,9 @@ public class InitController
             } catch (ConstraintViolationException e)
             {
                 logger.warn("A database constraint was violated while saving the new super admin.");
-                return new ResponseEntity<>("A database constraint was violated while saving the new super admin.", HttpStatus.BAD_REQUEST);
+                return new ResponseEntity<>(messageSource.getMessage("init.message.admin.constraintViolation", null, "A database constraint was violated while saving the new super admin", locale), HttpStatus.BAD_REQUEST);
             }
-            return new ResponseEntity<>("Successfully saved the new super admin. The settings are now being saved and the application is restarted.", HttpStatus.OK);
+            return new ResponseEntity<>(messageSource.getMessage("init.message.admin.savingAdminSuccess", null, "Successfully saved the new super admin. The settings are now being saved and the application is restarted.", locale) , HttpStatus.OK);
         }
     }
 }
